bca2a8e1df12d1abd67ffe569801cc6d6e2b3597
Robbie Luijben <diggingforfire@gmail.com>
2020-10-11 19:53:38 +0200

    Extra stuff

--- 1.2.hs
+++ 1.2.hs
@@ -1,3 +1,5 @@
 numSolutionsAbc a b c | b^2 - 4*a*c < 0   = 0
                       | b^2 - 4*a*c == 0  = 1
                       | b^2 - 4*a*c > 0   = 2
+
+numSolutionsAbc' a b c = signum (b^2 - 4*a*c) + 1
